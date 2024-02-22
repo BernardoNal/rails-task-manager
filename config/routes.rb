6d4d91f59b89633415d2a1ce8e719b6c35e0a21d
Bernardo Carvalho <nal-carvalho@hotmail.com>
2024-02-22 15:06:38 -0300

    atv 2

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,22 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
+  # get 'task' => 'task#index'
+  # get 'task/:id' => 'task#show'
+  # get 'task' => 'task#index'
+
+  get    "tasks",          to: "task#index"
+  # Create
+  get    "task/new",      to: "task#new", as: 'new_task'
+  post   "tasks",          to: "task#create"
+  # Read one - The `show` route needs to be *after* `new` route.
+  get    "task/:id",      to: "task#show" , as: 'task'
+  # Update
+  get    "task/:id/edit", to: "task#edit", as: 'edit_task'
+  patch  "task/:id",      to: "task#update"
+  # Delete
+  delete "task/:id",      to: "task#destroy"
+
   # Defines the root path route ("/")
   # root "posts#index"
 end
